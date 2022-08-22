d07d4d33e4613d01336871202ba5ca4e7e5709b3
aleksei.novikov <aleksei.novikov@onoffapp.com>
2022-08-22 16:42:17 +0300

    Added gorilla and locks for the in memory map

--- main.go
+++ main.go
@@ -2,31 +2,78 @@ package main
 
 import (
 	"errors"
+	"github.com/gorilla/mux"
+	"io"
 	"log"
 	"net/http"
+	"sync"
 )
 
 var ErrorNoSuchKey = errors.New("no such key")
-var store = make(map[string]string)
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
 
-func helloGoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello net/http!\n"))
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	value, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	value, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value))
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", helloGoHandler)
+	r := mux.NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	r.HandleFunc("/v1/{key}", keyValuePutHandler).Methods("PUT")
+	r.HandleFunc("/v1/{key}", keyValueGetHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func Put(key string, value string) error {
-	store[key] = value
+	store.Lock()
+	store.m[key] = value
+	store.Unlock()
+
 	return nil
 }
 
 func Get(key string) (string, error) {
-	value, ok := store[key]
+	store.RLock()
+	value, ok := store.m[key]
+	store.RUnlock()
+
 	if !ok {
 		return "", ErrorNoSuchKey
 	}
@@ -35,6 +82,9 @@ func Get(key string) (string, error) {
 }
 
 func Delete(key string) error {
-	delete(store, key)
+	store.Lock()
+	delete(store.m, key)
+	store.Unlock()
+
 	return nil
 }
